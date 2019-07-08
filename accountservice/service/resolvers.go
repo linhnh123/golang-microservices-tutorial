b8b4ff9a6f359eecb8e76b83f1c9dc9f79589643
linhnh <hanlinhqn@gmail.com>
2019-07-08 09:44:06 +0700

    Edit resolver

--- accountservice/service/resolvers.go
+++ accountservice/service/resolvers.go
@@ -1,6 +1,9 @@
 package service
 
-import "github.com/graphql-go/graphql"
+import (
+	"github.com/graphql-go/graphql"
+	internalmodel "github.com/linhnh123/golang-microservices-tutorial/accountservice/model"
+)
 
 type GraphQLResolvers interface {
 	AccountResolverFunc(p graphql.ResolveParams) (interface{}, error)
@@ -10,9 +13,14 @@ type LiveGraphQLResolvers struct {
 }
 
 func (gqlres *LiveGraphQLResolvers) AccountResolverFunc(p graphql.ResolveParams) (interface{}, error) {
-	account, err := fetchAccount(p.Context, p.Args["id"].(string))
-	if err != nil {
-		return nil, err
+	// account, err := fetchAccount(p.Context, p.Args["id"].(string))
+	// if err != nil {
+	// 	return nil, err
+	// }
+	account := internalmodel.Account{
+		Id:       "1234",
+		Name:     "Test name",
+		ServedBy: "Served",
 	}
 	return account, nil
 }
